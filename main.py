f93b34da9b6be6fcb41f66d7b3077bae66cb908a
austinvhuang <austinh@alum.mit.edu>
2024-07-16 11:56:36 -0400

    gpu.cpp website, initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from fasthtml.common import *
+import uvicorn
+
+# Shoelace components
+from fasthtml.components import (
+    Sl_icon,
+    Sl_button,
+    Sl_menu,
+    Sl_menu_item,
+    Sl_menu_item,
+    Sl_menu_item,
+    Sl_breadcrumb,
+    Sl_breadcrumb_item,
+    Sl_breadcrumb_item,
+    Sl_card,
+)
+
+app = FastHTMLWithLiveReload(
+    hdrs=picolink,
+)
+rt = app.route
+
+
+# For images, CSS, etc.
+@app.get("/{fname:path}.{ext:static}")
+async def static(fname: str, ext: str):
+    return FileResponse(f"{fname}.{ext}")
+
+
+@rt("/")
+def get():
+    return Title("FastHTML"), Body(
+        Br(),
+        Div(H1("gpu.cpp"), align="center", style="font-size: 10em"),
+        Div(H4("Portable GPU Compute Made Simple"), align="center"),
+        Div(
+            Img(src="images/shadertui.gif", style="height: 300px"),
+            Img(src="images/pendulum.gif", style="height: 300px"),
+            align="center",
+        ),
+        Div(
+            A("Blog Post", href="https://www.answer.ai/posts/2024-07-11--gpu-cpp.html"),
+            Br(),
+            A("GitHub", href="github.com/AnswerDotAI/gpu.cpp"),
+            Br(),
+            A("Discord", href="https://discord.gg/zmJVhXsC7f"),
+            align="center",
+        ),
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    # run_uv(fname=None, app=app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
