9424472c3205e2921346c52642b8db57f50d48a2
austinvhuang <austinh@alum.mit.edu>
2024-07-17 18:30:20 -0400

    splash page v1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Landing page Website for gpu.cpp
+
+To start the server, run:
+
+```
+python run.py
+```
--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 from fasthtml.common import *
 import uvicorn
 
-# Shoelace components
-from fasthtml.components import (
-    Sl_icon,
-    Sl_button,
-    Sl_menu,
-    Sl_menu_item,
-    Sl_menu_item,
-    Sl_menu_item,
-    Sl_breadcrumb,
-    Sl_breadcrumb_item,
-    Sl_breadcrumb_item,
-    Sl_card,
-)
-
 app = FastHTMLWithLiveReload(
     hdrs=picolink,
 )
@@ -29,34 +15,114 @@ async def static(fname: str, ext: str):
 
 @rt("/")
 def get():
-    return Title("FastHTML"), Body(
-        Nav(
-            Sl_icon("home"),
-            Menu('foo', 'Foo', '/foo'),
-            Menu('bar', 'Bar', '/bar'),
-            ),
+    font_size = 8.0
+    box_style = f"""
+        background-color: #22222266;
+        width: 20%;
+        margin: 10px;
+        padding: 8px;
+        border-radius: 3px;
+        color: white;
+        font-size: {font_size / 8.0}em;
+        text-align: center;
+        
+        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+        display: inline-block;
+        transition: background-color 0.3s ease;
+    """
+    hover_style = """
+        .hover-box:hover {
+            background-color: blue;
+        }
+    """
+    # center background image
+    body_style = """
+        background: linear-gradient(rgba(14, 14, 14, 0.87),
+                                    rgba(0, 0, 0, 0.94),
+                                    rgba(0, 0, 0, 0.9), 
+                                    rgba(0, 0, 0, 0.88), 
+                                    rgba(32, 32, 32, 0.88)), 
+                    url('images/shadertui2-small-crop2.gif');
+        background-size: cover;
+        background-position: center;
+        height: 100vh
+    """
 
-        Br(),
-        Div(H1("gpu.cpp"), align="center", style="font-size: 10em"),
-        Div(H4("Portable GPU Compute Made Simple"), align="center"),
+    return Title("FastHTML"), Body(
+            Div(
+                style="height: 16%;",
+            ),
         Div(
-            Img(src="images/shadertui.gif", style="height: 300px"),
-            Img(src="images/pendulum.gif", style="height: 300px"),
+            H1(
+                "gpu",
+                style=f"font-size: {font_size}em; color:#ee4c2cF0; display: inline;",
+            ),
+            H1(
+                ".",
+                # style=f"font-size: {font_size}em; display: inline; color:#0F0D8EFF;",
+                style=f"font-size: {font_size}em; display: inline; color:#FFFFFFF0;",
+            ),
+            H1(
+                "cpp",
+                style=f"font-size: {font_size}em; display: inline; color:#808080F0;",
+            ),
+            align="center",
+            padding="1020px",
+        ),
+        P(),
+        Div(
+            "Portable C++ GPU Compute using native WebGPU",
             align="center",
         ),
+        P(),
+        # Links
         Div(
-            A("Blog Post", href="https://www.answer.ai/posts/2024-07-11--gpu-cpp.html"),
+
+            Div(
+                A("GitHub", href="https://github.com/AnswerDotAI/gpu.cpp", style="text-decoration: none;"),
+                style=box_style + hover_style,
+                cls="hover-box",
+            ),
+            Div(
+                A(
+                    "Intro Blog",
+                    href="https://www.answer.ai/posts/2024-07-11--gpu-cpp.html", style="text-decoration: none;"
+                ),
+                style=box_style,
+                cls="hover-box",
+            ),
             Br(),
-            A("API Docs", href="api/index.html"),
-            Br(),
-            A("GitHub", href="https://github.com/AnswerDotAI/gpu.cpp"),
-            Br(),
-            A("Discord", href="https://discord.gg/zmJVhXsC7f"),
+            Div(
+                A("API Docs", href="api/index.html", style="text-decoration: none;"),
+                style=box_style,
+                cls="hover-box",
+            ),
+            Div(
+                A("Discord", href="https://discord.gg/zmJVhXsC7f", style="text-decoration: none;"),
+                style=box_style,
+                cls="hover-box",
+            ),
             align="center",
         ),
+        P(),
+        Div(
+                # link color white
+                A("Answer.AI", href="https://www.answer.ai", style=f"text-decoration: none; color: #33333399;"),
+            align="center",
+        ),
+        # set height to take up the full window
+        style=body_style,
+        cls = "crt",
+        # style = "crt { animation: fadeIn; } @keyframes fadeIn { 0% { opacity: 0; } 100% { opacity: 1; } } animation: fadeIn 2s;",
+        # style = ".crt { animation: flicker; } @keyframes flicker { 0%, 100% { opacity: 0.6; } 50% { opacity: 0.8; } } @keyframes fadeIn { 0% { opacity: 0; } 100% { opacity: 1; } } animation: fadeIn 2s;",
     )
 
 
 if __name__ == "__main__":
-    # uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
-    run_uv(fname=None, app="app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
+    run_uv(
+        fname=None,
+        app="app",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8000)),
+        reload=True,
+    )
