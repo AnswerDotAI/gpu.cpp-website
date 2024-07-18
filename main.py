33fe60348f8a4ef6a493a98e920874fd212f8819
austinvhuang <austinh@alum.mit.edu>
2024-07-18 11:52:23 -0400

    divs inside links

--- main.py
+++ main.py
@@ -2,39 +2,46 @@ from fasthtml.common import *
 from functools import cache
 import uvicorn
 
-app = FastHTMLWithLiveReload(
-    hdrs=picolink,
-)
+TARGET = "debug"
+
+if TARGET == "release":
+    app = FastHTML(hdrs=picolink)
+else:
+    app = FastHTMLWithLiveReload(hdrs=picolink)
+
 rt = app.route
 
 
-@app.get("/{fname:path}.{ext:static}")
-async def static(fname: str, ext: str):
-    return FileResponse(f"{fname}.{ext}")
+@app.get("/api/{fname:path}.{ext:static}")
+async def api(fname: str, ext: str):
+    return FileResponse(f"api/{fname}.{ext}")
+
+
+@app.get("/images/{fname:path}.{ext:static}")
+async def images(fname: str, ext: str):
+    return FileResponse(f"images/{fname}.{ext}")
 
 
 @cache
 def page():
-    font_size = 8.0
-    hero_style = f"font-size: {font_size}em; display: inline; font-family: 'Helvetica';"
+    font_family = "font-family: 'Helvetica';"
+    hero_style = f"font-size: 7.6em; display: inline; {font_family}"
+
     box_style = f"""
         background-color: #ffffff12;
-        width: 15em;
+        width: 8em;
         margin: 12px;
         padding: 8px;
         border-radius: 4px;
         backdrop-filter: blur(3px);
         -webkit-backdrop-filter: blur(2px);
-        color: white;
-        font-size: {font_size / 8.0}em;
         text-align: center;
         box-shadow: 0 0 20px 0 rgba(48, 48, 48, 0.3);
         display: inline-block;
-        transition: background-color 0.3s ease;
     """
     body_style = """
         background: linear-gradient(rgba(14, 14, 14, 0.86),
-                                    rgba(0, 0, 0, 0.95),
+                                    rgba(0, 0, 0, 1.0),
                                     rgba(0, 0, 0, 0.92), 
                                     rgba(0, 0, 0, 0.89), 
                                     rgba(28, 28, 28, 0.80)), 
@@ -43,10 +50,12 @@ def page():
         background-position: center;
         height: 100vh
     """
-    link_style = "text-decoration: none; color: #44449EFF; font-size: 1.3em;  font-family: 'Helvetica';"
+    link_style = (
+        "text-decoration: none; color: #44449EFF; font-size: 1.58em; {font_family}"
+    )
     return Title("gpu.cpp"), Body(
         Div(
-            style="height: 16%;",
+            style="height: 12%;",
         ),
         Div(
             H1(
@@ -55,64 +64,65 @@ def page():
             ),
             H1(
                 ".",
-                style=hero_style + " color:#BBBBBBFF;",
+                style=hero_style + " color:#808080FF;",
             ),
             H1(
                 "cpp",
-                style=hero_style + " color:#808080FF;",
+                style=hero_style + " color:#B4B4B4FF;",
             ),
             align="center",
         ),
         P(),
+        Br(),
+        Br(),
         Div(
             "Portable C++ GPU Compute using WebGPU",
             align="center",
-            style=f"text-decoration: none; color: #88888899; font-size: 1.58em; font-family: 'Helvetica';",
+            style=f"text-decoration: none; color: #808080FF; font-size: 1.58em; {font_family}",
         ),
         P(),
         Div(
-            Div(
-                A(
-                    # Img(src="images/github.svg", style="filter: invert(1);"),
+            A(
+                Div(
                     "GitHub",
-                    href="https://github.com/AnswerDotAI/gpu.cpp",
-                    style=link_style,
+                    style=box_style + link_style,
                 ),
-                style=box_style,
+                href="https://github.com/AnswerDotAI/gpu.cpp",
+                style="text-decoration: none;",
             ),
-            Div(
-                A(
+            A(
+                Div(
                     "Blog Post",
-                    href="https://www.answer.ai/posts/2024-07-11--gpu-cpp.html",
-                    style=link_style,
+                    style=box_style + link_style,
                 ),
-                style=box_style,
+                href="https://www.answer.ai/posts/2024-07-11--gpu-cpp.html",
+                style="text-decoration: none;",
             ),
             Br(),
-            Div(
-                A(
+            A(
+                Div(
                     "API Docs",
-                    href="api/index.html",
-                    style=link_style,
+                    style=box_style + link_style,
                 ),
-                style=box_style,
+                href="api/index.html",
+                style="text-decoration: none;",
             ),
-            Div(
-                A(
+            A(
+                Div(
                     "Discord",
-                    href="https://discord.gg/zmJVhXsC7f",
-                    style=link_style,
+                    style=box_style + link_style,
                 ),
-                style=box_style,
+                href="https://discord.gg/zmJVhXsC7f",
+                style="text-decoration: none;",
             ),
             align="center",
         ),
         P(),
         Div(
             A(
-                "Answer.AI",
+                "— Answer.AI —",
                 href="https://www.answer.ai",
-                style=f"text-decoration: none; color: #88888899; font-size: 1.58em ; font-family: 'Helvetica';",
+                style=f"text-decoration: none; color: #303030FF; font-size: 1.58em ; {font_family}",
             ),
             align="center",
         ),
