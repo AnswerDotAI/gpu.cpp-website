104eb561cfafe1acd1eaa7372e3868f9934df0e1
austinvhuang <austinh@alum.mit.edu>
2024-07-21 10:27:36 -0400

    add new api doc files

---
+++ api/structgpu_1_1_callback_data.js
@@ -0,0 +1,8 @@
+var structgpu_1_1_callback_data =
+[
+    [ "buffer", "structgpu_1_1_callback_data.html#aac088e4df9a823a173f8dc7d592959de", null ],
+    [ "bufferSize", "structgpu_1_1_callback_data.html#aba171d7b560baa36b8b7a539ffd1fd29", null ],
+    [ "future", "structgpu_1_1_callback_data.html#a329b90e9a2950a603d03b02c8abc4c11", null ],
+    [ "output", "structgpu_1_1_callback_data.html#a7f623f63c84beb5aef93e7dd95552bd4", null ],
+    [ "promise", "structgpu_1_1_callback_data.html#a6c9c8e507b4e1cd865e32313edeeaaa3", null ]
+];
---
+++ api/structgpu_1_1_copy_data.js
@@ -0,0 +1,7 @@
+var structgpu_1_1_copy_data =
+[
+    [ "commandBuffer", "structgpu_1_1_copy_data.html#a2221c46fc86dfce06dcea4c72ffb4ee1", null ],
+    [ "future", "structgpu_1_1_copy_data.html#af607412184396f25b8b182bd24a4202c", null ],
+    [ "promise", "structgpu_1_1_copy_data.html#a92c9c93be52bf25997cf50d776929d22", null ],
+    [ "readbackBuffer", "structgpu_1_1_copy_data.html#a5118ab31b5c3062671940856aa7fcee8", null ]
+];
